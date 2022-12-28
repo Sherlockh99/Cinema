9204288073b54391967167862c44bf33360bf411
Sherlock <sherlockh989@gmail.com>
2022-12-28 19:44:36 +0200

    Settings MainActivity.kt

--- app/src/main/java/com/sherlock/gb/kotlin/cinema/view/MainActivity.kt
+++ app/src/main/java/com/sherlock/gb/kotlin/cinema/view/MainActivity.kt
@@ -2,17 +2,34 @@ package com.sherlock.gb.kotlin.cinema.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.sherlock.gb.kotlin.cinema.R
+import com.sherlock.gb.kotlin.cinema.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val mainFragment = MainFragment()
+        setFragment(mainFragment)
+
+        binding.bottomNavigationView.setOnNavigationItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> setFragment(mainFragment)
+            }
+            true
         }
     }
+
+    private fun setFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.flFragment, fragment)
+            commit()
+        }
 }
