7dfb97e77f6b36856d89bca5359935ff7a551444
Sherlock <sherlockh989@gmail.com>
2023-01-01 13:44:18 +0200

    Added Descriptions

--- app/src/main/java/com/sherlock/gb/kotlin/cinema/model/Movie.kt
+++ app/src/main/java/com/sherlock/gb/kotlin/cinema/model/Movie.kt
@@ -2,9 +2,7 @@ package com.sherlock.gb.kotlin.cinema.model
 
 data class Movie(
     val movie_title: String,
-    val release_date: String,
+    val original_title: String,
     val year: Int,
     val picture: Int = 0
 )
-
-fun getDefaultMovie() = Movie("Форсаж","27.11.2018",2018)
